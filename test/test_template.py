12c2c12d2cb8b239a7028b4e77d10985f7f18ad6
Ghislain Bernard <ghislain.bernard@gmail.com>
2023-01-13 12:33:32 +0100

    Insert 'test/test_template.py'

---
+++ test/test_template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
+
+import unittest
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+import template
+
+class TestTemplate(unittest.TestCase):
+
+  def test_project(self):
+    self.assertTrue(hasattr(template, 'PROJECT_NAME'))
+    self.assertTrue(hasattr(template, 'PROJECT_VERSION_MAJOR'))
+    self.assertTrue(hasattr(template, 'PROJECT_VERSION_MINOR'))
+    self.assertTrue(hasattr(template, 'PROJECT_VERSION_PATCH'))
+    self.assertTrue(hasattr(template, 'PROJECT_VERSION'))
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+if __name__ == '__main__':
+  unittest.main()
+
+#--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
