31c73c96559aef41b6dff1b13f8090077d17e1af
Ghislain Bernard <ghislain.bernard@gmail.com>
2023-01-13 12:33:32 +0100

    Insert 'template.py'

---
+++ template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
+
+PROJECT_NAME = 'template'
+PROJECT_VERSION_MAJOR = '0'
+PROJECT_VERSION_MINOR = '0'
+PROJECT_VERSION_PATCH = '1'
+PROJECT_VERSION = PROJECT_VERSION_MAJOR + '.' + PROJECT_VERSION_MINOR + '.' + PROJECT_VERSION_PATCH
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+if __name__ == '__main__':
+  print(PROJECT_NAME + '@' + PROJECT_VERSION)
+
+#--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
